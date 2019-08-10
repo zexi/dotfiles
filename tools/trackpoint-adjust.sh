de97b64620de0ae82d76e584b886cc70e110657d
Zexi <zexi.li@qq.com>
2019-08-11 01:55:45 +0800

    sway config

--- tools/trackpoint-adjust.sh
+++ tools/trackpoint-adjust.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-SPEED=230
-SENSITIVITY=160
+SPEED=200
+SENSITIVITY=200
 
 echo -n $SPEED > /sys/devices/platform/i8042/serio1/serio2/speed
 echo -n $SENSITIVITY > /sys/devices/platform/i8042/serio1/serio2/sensitivity
