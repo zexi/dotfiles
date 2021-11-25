ad51d04863e436236ba23a69e61490a3b3faacfe
Zexi Li <zexi.li@icloud.com>
2021-11-25 21:23:37 +0800

    update i3 config

---
+++ i3/arandr/g14-4k-2k.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 2560x1440 --pos 3840x1506 --rotate normal --output HDMI-1 --mode 2560x1440 --pos 1280x2160 --rotate normal --output DP-1-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1-1 --off
--- tools/trackpoint-adjust.sh
+++ tools/trackpoint-adjust.sh
@@ -5,3 +5,7 @@ SENSITIVITY=175
 
 echo -n $SPEED > /sys/devices/platform/i8042/serio1/serio2/speed
 echo -n $SENSITIVITY > /sys/devices/platform/i8042/serio1/serio2/sensitivity
+
+/usr/bin/xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Tapping Enabled" 1
+/usr/bin/xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Natural Scrolling Enabled" 1
+/usr/bin/xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Accel Speed" 0.5
