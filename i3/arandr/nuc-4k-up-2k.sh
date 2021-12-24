7773669d2e8336030ab3338c962383d29bb7d542
Zexi Li <zexi.li@icloud.com>
2021-12-24 12:32:28 +0800

    i3: add nuc xrandr layout

---
+++ i3/arandr/nuc-4k-up-2k.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP1 --primary --mode 2560x1440 --pos 640x2160 --rotate normal --output DP2 --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off
