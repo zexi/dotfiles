877c1588d6ad7ad6d802cabe352a6e277e6058cf
Zexi Li <zexi.li@icloud.com>
2022-04-16 02:37:07 +0800

    nvim: add indent-blankline

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,11 +1,11 @@
 vim.cmd [[
 try
-  " colorscheme darkplus
-  colorscheme apprentice
-  highlight Comment ctermbg=NONE ctermfg=240 cterm=NONE guibg=NONE guifg=#808080 gui=NONE
+  colorscheme darkplus
+  " colorscheme apprentice
+  " highlight Comment ctermbg=NONE ctermfg=240 cterm=NONE guibg=NONE guifg=#808080 gui=NONE
   " highlight CursorLine ctermbg=236 ctermfg=NONE cterm=NONE guibg=#903040 guifg=NONE gui=NONE
-  highlight LineNr guibg=NONE
-  highlight VertSplit ctermbg=NONE guibg=NONE
+  " highlight LineNr guibg=NONE
+  " highlight VertSplit ctermbg=NONE guibg=NONE
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
