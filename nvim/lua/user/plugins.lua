a1695c05636e5870e90ab7b87717fa890a60a404
Zexi Li <zexi.li@icloud.com>
2022-04-15 16:19:15 +0800

    nvim: adjust colorscheme

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -62,6 +62,7 @@ return packer.startup(function(use)
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
+  use "romainl/Apprentice"
 
   -- cmp plugins
   use "neovim/nvim-lspconfig" -- enable LSP
