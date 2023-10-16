b59203af09cb85dc9ca1b36658a92cada133be77
Zexi Li <zexi.li@icloud.com>
2023-10-16 12:06:48 +0800

    nvim: coc reconfiguration

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -44,9 +44,9 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim"    -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim"  -- Useful lua functions used ny lots of plugins
-  use "windwp/nvim-autopairs"  -- Autopairs, integrates with both cmp and treesitter
+  -- use "windwp/nvim-autopairs"  -- Autopairs, integrates with both cmp and treesitter
   -- use 'm4xshen/autoclose.nvim'
-  -- use 'cohama/lexima.vim'
+  use 'cohama/lexima.vim'
 
   -- Easily comment stuff
   use { "numToStr/Comment.nvim", tag = 'v0.6' }
@@ -76,29 +76,32 @@ return packer.startup(function(use)
     requires = "rktjmp/lush.nvim"
   }
 
-  -- LSP and completion
-  -- use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-  -- use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
-  -- use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-  -- cmp plugins
-  use "neovim/nvim-lspconfig" -- enable LSP
-  -- coq completion
-  -- use { "zexi/coq_nvim", branch = 'stop-cmp-syms' }
-  -- -- use "ms-jpq/coq.artifacts"
-  use { "lukas-reineke/lsp-format.nvim", tag = "v2.2.3" }
-  use { "folke/lua-dev.nvim" }
-  -- snippets
-  use "L3MON4D3/LuaSnip"             --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
-  -- cmp completion
-  use "hrsh7th/nvim-cmp"             -- The completion plugin
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-buffer"           -- buffer completions
-  use "hrsh7th/cmp-path"             -- path completions
-  use "hrsh7th/cmp-cmdline"          -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip"     -- snippet completions
-  -- use {'neoclide/coc.nvim', branch = 'release'}
-  -- use 'fannheyward/telescope-coc.nvim'
+  -- -- LSP and completion
+  -- -- use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  -- -- use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+  -- -- use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  -- -- cmp plugins
+  -- use "neovim/nvim-lspconfig" -- enable LSP
+  -- -- coq completion
+  -- -- use { "zexi/coq_nvim", branch = 'stop-cmp-syms' }
+  -- -- -- use "ms-jpq/coq.artifacts"
+  -- use { "lukas-reineke/lsp-format.nvim", tag = "v2.2.3" }
+  -- use { "folke/lua-dev.nvim" }
+  -- -- snippets
+  -- use "L3MON4D3/LuaSnip"             --snippet engine
+  -- use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  -- -- cmp completion
+  -- use "hrsh7th/nvim-cmp"             -- The completion plugin
+  -- use "hrsh7th/cmp-nvim-lsp"
+  -- use "hrsh7th/cmp-buffer"           -- buffer completions
+  -- use "hrsh7th/cmp-path"             -- path completions
+  -- use "hrsh7th/cmp-cmdline"          -- cmdline completions
+  -- use "saadparwaiz1/cmp_luasnip"     -- snippet completions
+
+  -- coc plugins
+  use { 'neoclide/coc.nvim', branch = 'release' }
+  use 'fannheyward/telescope-coc.nvim'
+
   use "buoto/gotests-vim"
 
   -- Telescope
--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -5,7 +5,7 @@ end
 
 configs.setup {
   -- ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = { "go", "lua", "rust", "vue" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { "go", "python", "lua", "rust", "vue", "c", "lua" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
@@ -17,9 +17,14 @@ configs.setup {
     disable = { "" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
   },
-  indent = { enable = true, disable = { "yaml" } },
-  context_commentstring = {
-    enable = false,
-    enable_autocmd = false,
-  },
+  -- indent = { enable = true, disable = { "yaml" } },
+  -- context_commentstring = {
+  --   enable = false,
+  --   enable_autocmd = false,
+  -- },
 }
+
+-- folding: https://github.com/nvim-treesitter/nvim-treesitter#folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr="nvim_treesitter#foldexpr()"
+vim.opt.foldenable=false
