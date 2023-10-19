6dd9843350cd83a9a08ce0da651ed3692a217b4f
Zexi Li <zexi.li@icloud.com>
2023-10-19 17:00:54 +0800

    nvim: telescope adds file_ignore_patterns

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -7,6 +7,7 @@ local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
+    file_ignore_patterns = { "^./.git/", "^node_modules/", "^vendor/" },
 
     -- prompt_prefix = " ",
     -- selection_caret = " ",
@@ -88,10 +89,10 @@ telescope.setup {
   extensions = {
     -- Your extension configuration goes here:
     fzf = {
-      fuzzy = true, -- false will only do exact matching
+      fuzzy = true,                   -- false will only do exact matching
       override_generic_sorter = true, -- override the generic sorter
-      override_file_sorter = true, -- override the file sorter
-      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      override_file_sorter = true,    -- override the file sorter
+      case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
       -- the default case_mode is "smart_case"
     }
     -- extension_name = {
