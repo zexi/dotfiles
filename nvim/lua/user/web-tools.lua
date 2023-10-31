84f457e885ee8abf297b04c6a058e320ea3df362
Zexi Li <zexi.li@icloud.com>
2023-10-31 23:06:34 +0800

    neovim: remove web-tools rename '.' mapping

--- nvim/lua/user/web-tools.lua
+++ nvim/lua/user/web-tools.lua
@@ -1,7 +1,7 @@
 require 'web-tools'.setup({
   keymaps = {
-    rename = nil,         -- by default use same setup of lspconfig
-    repeat_rename = '.',  -- . to repeat
+    rename = nil, -- by default use same setup of lspconfig
+    -- repeat_rename = '.',  -- . to repeat
   },
   hurl = {                -- hurl default
     show_headers = false, -- do not show http headers
